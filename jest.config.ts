0bafa0c9aea419a468e109549cb7f5f9bffe58f2
Leonardo Monteiro <leonardo.silva@semantix.ai>
2024-01-26 16:18:10 -0300

    chore: add jest

---
+++ jest.config.ts
@@ -0,0 +1,13 @@
+import type { Config } from 'jest'
+
+const config: Config = {
+  roots: ['<rootDir>/src'],
+  collectCoverage: true,
+  collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
+  coverageDirectory: 'coverage',
+  transform: {
+    '.+\\.ts$': 'ts-jest'
+  }
+}
+
+export default config
