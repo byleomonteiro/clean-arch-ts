b127c90cf9147b1ce90756eaa360bd5243dd39f9
Léo Monteiro <byleomonteiro@outlook.com>
2024-03-14 17:54:55 -0300

    chore: add jest configs and scripts

--- jest.config.ts
+++ jest.config.ts
@@ -2,7 +2,6 @@ import type { Config } from 'jest'
 
 const config: Config = {
   roots: ['<rootDir>/src'],
-  collectCoverage: true,
   collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
   coverageDirectory: 'coverage',
   transform: {
