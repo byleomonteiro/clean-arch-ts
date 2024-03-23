19872934914b93cf21091a9ccfb7490bf526f73a
Léo Monteiro <byleomonteiro@outlook.com>
2024-03-23 17:19:36 -0300

    test: ensure AccountMongoRepository return an account on success

--- src/utils/infra/db/mongodb/account-repository/account.spec.ts
+++ src/utils/infra/db/mongodb/account-repository/account.spec.ts
@@ -7,12 +7,21 @@ describe('Account Mongo Repository', () => {
     await MongoHelper.connect(mongoUrl)
   })
 
+  beforeEach(async () => {
+    const accountCollection = MongoHelper.getCollection('accounts')
+    await accountCollection.deleteMany({})
+  })
+
   afterAll(async () => {
     await MongoHelper.disconnect()
   })
 
+  const makeSut = (): AccountMongoRepository => {
+    return new AccountMongoRepository()
+  }
+
   test('Should return an account on success', async () => {
-    const sut = new AccountMongoRepository()
+    const sut = makeSut()
 
     const account = await sut.add({
       name: 'any_name',
@@ -21,7 +30,7 @@ describe('Account Mongo Repository', () => {
     })
 
     expect(account).toBeTruthy()
-    expect(account).toBeTruthy()
+    expect(account.id).toBeTruthy()
     expect(account.name).toBe('any_name')
     expect(account.email).toBe('any_email@mail.com')
     expect(account.password).toBe('any_password')
